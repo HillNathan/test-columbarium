e71da07181d17426b4ca8717e2d0cb6dac40cc64
Nathan Hill <pjsstp@gmail.com>
2020-10-16 15:11:52 -0400

    pulling back - got host error

--- server-config/index.js
+++ server-config/index.js
@@ -1,12 +1,11 @@
+// require("dotenv").config();
+
 // SET EXPRESS ENVIRONMENT
 const express = require("express");
 const app = express();
 
 // imports session, http=proxy, logger, and passport to the server
 const middleware = require("../middleware");
-
-// Set up our http-proxy
-// app.use(middleware.myProxy)
 
 // Set variables for our express environment
 app.use(express.urlencoded({ extended: true }));
@@ -20,15 +19,6 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
 
-// We need to use sessions to keep track of our user's login status
-app.use(
-  middleware.session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-);
-app.use(middleware.passport.initialize());
-app.use(middleware.passport.session());
-
-// include all of our routes from the routes module to our express environment
 require('../routes')(app);
 
-// export our express environment back to the server
 module.exports = app;
