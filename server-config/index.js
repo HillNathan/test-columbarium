b47c9aded65a265f95ae46777c1b370522641035
Nathan Hill <pjsstp@gmail.com>
2020-10-17 10:46:31 -0400

    added passport middleware and config setting

--- server-config/index.js
+++ server-config/index.js
@@ -14,6 +14,14 @@ app.use(express.json());
 // ADD WINSTON LOGGER MIDDLEWARE TO SERVER
 app.use(middleware.logger);
 
+// We need to use sessions to keep track of our user's login status
+// app.use(
+//   middleware.session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+// );
+app.use(middleware.passport.initialize());
+app.use(middleware.passport.session());
+
+
 // check to see if we are using the production version of the app
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./server-config')
 const port = process.env.PORT || 5000;
 
+const db = require("./models");
+
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
